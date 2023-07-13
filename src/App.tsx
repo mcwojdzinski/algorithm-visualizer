10f81ea4b83163e22b8bce72d76a17b10fd89a4c
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-07-13 13:51:28 +0200

    Reorganize structure of the project with app.tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,144 @@
-import './App.css'
-import { Button } from './components/Button/Button.tsx'
-import { Bar } from './components/Bar/Bar.tsx'
-import { generateSetOfNumbers } from './helpers/generateSetOfNumbers.ts'
-import { ChartWrapper } from './components/ChartWrapper/ChartWrapper.tsx'
-import { Header } from './components/Header/Header.tsx'
-import { useState } from 'react'
-import { quickSort } from './helpers/quickSort.ts'
+import { useState, useEffect, ChangeEvent } from 'react'
 
-function App() {
-    const [numbersArray, setNumbersArray] = useState<number[]>(
-        generateSetOfNumbers()
-    )
+import Controls from './components/Controls/Controls.tsx'
+import Legends from './components/Legends/Legends'
+import Bar from './components/Bar/Bar.tsx'
 
-    const regenerateNumbers = () => {
-        setNumbersArray(generateSetOfNumbers())
+// Algorithms
+import quickSort from './helpers/algorithms/quickSort.ts'
+import styled from 'styled-components'
+
+const Wrapper = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+`
+
+const Header = styled.h1`
+    font-size: 50px;
+    color: white;
+    text-transform: capitalize;
+    letter-spacing: 2px;
+    margin: 20px;
+`
+
+const App = () => {
+    const [algo, setAlgo] = useState('quickSort')
+    const [len, setLength] = useState(30)
+    const [blocks, setBlocks] = useState([])
+    const [sorting, setSorting] = useState(false)
+    const [completed, setCompleted] = useState(true)
+    const [speed, setSpeed] = useState(250)
+    const [compare, setCompare] = useState([])
+    const [swap, setSwap] = useState([])
+    const [sortedIndex, setSortedIndex] = useState([])
+
+    // Generating shuffled array from 1 to chosen length
+    const generateRandomArray = (len: number) => {
+        setCompleted(false)
+        setSorting(false)
+        setSortedIndex([])
+
+        const randomArray = Array.from(Array(len + 1).keys()).slice(1)
+
+        for (let i = randomArray.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i - 1))
+            const temp = randomArray[i]
+
+            randomArray[i] = randomArray[randomIndex]
+            randomArray[randomIndex] = temp
+        }
+
+        setBlocks(randomArray)
     }
 
-    const runQuickSort = () => {
-        setNumbersArray(quickSort(numbersArray))
+    // Generating random array every time the length is changed by th user
+    useEffect(() => {
+        generateRandomArray(len)
+    }, [len, algo])
+
+    // setting the selected algorithm
+    const handleAlgo = (event: ChangeEvent<HTMLInputElement>) => {
+        setAlgo(event.target.value)
+    }
+
+    // handling the length of the array
+    const handleLength = (event: ChangeEvent<HTMLInputElement>) => {
+        setLength(Number(event.target.value))
+    }
+
+    // handling the speed of sorting
+    const handleSpeed = (event: ChangeEvent<HTMLInputElement>) => {
+        setSpeed(Math.ceil(400 / Number(event.target.value)))
+    }
+
+    // Sorting according to the algorithm
+    const handleSort = () => {
+        const sortAccOrder = (order: any[]) => {
+            ;(function loop(i: number) {
+                setTimeout(function () {
+                    const [j, k, arr, index] = order[i]
+                    setCompare([j, k])
+                    setSwap([])
+
+                    if (index !== null) {
+                        setSortedIndex((prevState: number[]) => {
+                            return [...prevState, index]
+                        })
+                    }
+
+                    if (arr) {
+                        setBlocks(arr)
+                        if (j !== null || k != null) setSwap([j, k])
+                    }
+
+                    if (++i < order.length) {
+                        loop(i)
+                    } else {
+                        setSorting(false)
+                        setCompleted(true)
+                    }
+                }, speed)
+            })(0)
+        }
+
+        setSorting(true)
+
+        algo === 'quickSort'
+            ? sortAccOrder(quickSort(blocks))
+            : (() => {
+                  setSorting(false)
+                  setCompleted(true)
+              })()
     }
 
     return (
-        <>
-            <Header>Algorithm Visualizer</Header>
-            <Button onClick={regenerateNumbers}>Generate your numbers</Button>
-            <Button onClick={runQuickSort}>Quick sort</Button>
-            <ChartWrapper>
-                {numbersArray.map((number, index) => (
-                    <Bar heightValue={number} key={index}>
-                        {number}
-                    </Bar>
-                ))}
-            </ChartWrapper>
-        </>
+        <Wrapper>
+            <Header>Sorting algorithm visualization </Header>
+            <Controls
+                generateRandomArray={() => generateRandomArray(len)}
+                handleLength={handleLength}
+                handleSpeed={handleSpeed}
+                handleAlgo={handleAlgo}
+                handleSort={handleSort}
+                sorting={sorting}
+                completed={completed}
+                len={len}
+                speed={speed}
+                algo={algo}
+            />
+
+            <Bar
+                blocks={blocks}
+                compare={sorting && compare}
+                swap={sorting && swap}
+                sorted={sortedIndex}
+            />
+
+            <Legends />
+        </Wrapper>
     )
 }
 
---
+++ src/helpers/algorithms/quickSort.ts
@@ -0,0 +1,60 @@
+let order: Array<
+    [number | null, number | null, number[] | null, number | null]
+> = []
+
+const swap = (arr: number[], i: number, j: number) => {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+const partition = (dupBlocks: number[], l: number, r: number): number => {
+    const pivot = l
+    let j = l
+
+    for (let i = l + 1; i <= r; i++) {
+        order.push([i, pivot, null, null])
+        if (dupBlocks[i] < dupBlocks[pivot]) {
+            j += 1
+            swap(dupBlocks, i, j)
+            order.push([i, j, dupBlocks.slice(), null])
+        }
+    }
+
+    swap(dupBlocks, pivot, j)
+    order.push([pivot, j, dupBlocks.slice(), null])
+    order.push([null, null, null, j])
+    return j
+}
+
+const quickSortHelper = (dupBlocks: number[], l: number, r: number) => {
+    if (l >= r) {
+        if (l === r) order.push([null, null, null, l])
+        return
+    }
+
+    const pivot = l + Math.floor(Math.random() * (r - l))
+
+    swap(dupBlocks, l, pivot)
+    order.push([l, pivot, dupBlocks.slice(), null])
+
+    const m = partition(dupBlocks, l, r)
+
+    quickSortHelper(dupBlocks, l, m - 1)
+    quickSortHelper(dupBlocks, m + 1, r)
+
+    return
+}
+
+const quickSort = (
+    blocks: number[]
+): Array<[number | null, number | null, number[] | null, number | null]> => {
+    const dupBlocks = blocks.slice() // Copying blocks array
+    order = []
+
+    quickSortHelper(dupBlocks, 0, dupBlocks.length - 1)
+
+    return order
+}
+
+export default quickSort
